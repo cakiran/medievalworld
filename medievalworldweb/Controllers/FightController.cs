a4e2d92aae494dfe0c6ac90f43de45e17d5b800d
cakiran <cakiran@gmail.com>
2025-07-24 23:13:14 -0400

    New effective attack method added

--- medievalworldweb/Controllers/FightController.cs
+++ medievalworldweb/Controllers/FightController.cs
@@ -32,6 +32,17 @@ namespace medievalworldweb.Controllers
             }
             return Ok(response);
         }
+
+        public async Task<IActionResult> EffectiveWeaponAttack([FromBody] WeaponAttackDto weaponAttackDto)
+        {
+            var response = await _fightService.WeaponAttack(weaponAttackDto);
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+
         [HttpPost, Route("Skill")]
         public async Task<IActionResult> SkillAttack([FromBody] SkillAttackDto skillAttackDto)
         {
